30f34ba7622c5cc19d08e88d014ddef6caace630
Alemeshet Alemu <alemyis@gmail.com>
2011-03-27 14:09:31 -0700

    Add matching type of question

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -9,6 +9,13 @@ class Categorization < ActiveRecord::Base
   #accepts_nested_attributes_for :answers, :reject_if => lambda { |a| a[:email].blank? || a[:nickname].blank? || a[:response].blank? }, :allow_destroy => true
   accepts_nested_attributes_for :answers, :reject_if => lambda { |a| a[:response].blank? }, :allow_destroy => true
   
+  def misale_tails
+    tails = []
+    self.challenge.misales.each do |misale|
+      tails.push misale.tail
+    end
+    tails.shuffle
+  end
   
   #validates_associated :answers
 end
